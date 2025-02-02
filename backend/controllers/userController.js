b12b5b2fbf31abd1197d3d679aa36ce80fcdf48e
23046025-NgYiLee <23046025@myrp.edu.sg>
2025-02-03 02:01:23 +0800

    initial

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,9 @@
-// userController.js
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
+// Function to create a JWT token
 const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '30d' })
 }
 
 // login a user
@@ -11,13 +11,16 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body
 
   try {
+    // Attempt to find and login the user
     const user = await User.login(email, password)
 
-    // create a token
+    // If login is successful, create a token
     const token = createToken(user._id)
 
-    res.status(200).json({ email, token })
+    // Respond with user email and token
+    res.status(200).json({ user: { email: user.email }, token })
   } catch (error) {
+    // If error occurs, send a 400 status with the error message
     res.status(400).json({ error: error.message })
   }
 }
@@ -27,13 +30,16 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body
 
   try {
+    // Attempt to signup the user
     const user = await User.signup(email, password)
 
-    // create a token
+    // If signup is successful, create a token
     const token = createToken(user._id)
 
-    res.status(200).json({ email, token })
+    // Respond with user email and token
+    res.status(200).json({ user: { email: user.email }, token })
   } catch (error) {
+    // If error occurs, send a 400 status with the error message
     res.status(400).json({ error: error.message })
   }
 }
