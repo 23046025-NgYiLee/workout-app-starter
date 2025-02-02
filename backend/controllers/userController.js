fa1825c1f40ed16603060a341d407e555ab0cb5b
23046025-NgYiLee <23046025@myrp.edu.sg>
2025-02-03 01:53:13 +0800

    initial

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,25 @@
+// userController.js
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// login a user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 // signup a user
@@ -15,12 +32,10 @@ const signupUser = async (req, res) => {
     // create a token
     const token = createToken(user._id)
 
-    // Send back the email and token to the client
-    res.status(200).json({ email: user.email, token })  // Returning user email and token
-
+    res.status(200).json({ email, token })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
 }
 
-module.exports = { signupUser }
+module.exports = { signupUser, loginUser }
