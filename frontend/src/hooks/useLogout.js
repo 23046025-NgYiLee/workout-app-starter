a23b76cdec308e8476e0d18379060982a2a97e6b
23046025-NgYiLee <23046025@myrp.edu.sg>
2025-02-03 15:59:39 +0800

    initial

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,37 +1,18 @@
-import React, { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
+import { useWorkoutsContext } from './useWorkoutsContext'
 
-export const useSignup = () => {
-  const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(null)
+export const useLogout = () => {
   const { dispatch } = useAuthContext()
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext()
 
-  const signup = async (email, password) => {
-    setIsLoading(true)
-    setError(null)
+  const logout = () => {
+    // remove user from storage
+    localStorage.removeItem('user')
 
-    const response = await fetch('/api/user/signup', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({ email, password })
-    })
-    const json = await response.json()
-
-    if (!response.ok) {
-      setIsLoading(false)
-      setError(json.error)
-    }
-    if (response.ok) {
-      // save the user to local storage
-      localStorage.setItem('user', JSON.stringify(json))
-
-      // update the auth context
-      dispatch({type: 'LOGIN', payload: json})
-
-      // update loading state
-      setIsLoading(false)
-    }
+    // dispatch logout action
+    dispatch({ type: 'LOGOUT' })
+    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
   }
 
-  return { signup, isLoading, error }
+  return { logout }
 }
