23403fd1041eee0acb161ee0f771c518b34fd410
23046025-NgYiLee <23046025@myrp.edu.sg>
2025-02-03 15:37:38 +0800

    initial

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
 
 const app = express();
-
 app.use(express.json());
 
 app.use((req, res, next) => {
