24802cfd3561642a3d19fb78bd856cf8269bb781
23046025-NgYiLee <23046025@myrp.edu.sg>
2025-02-03 13:13:24 +0800

    initial

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 import { useAuthContext } from '../hooks/useAuthContext'
 
